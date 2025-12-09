c4d3ca0e3d07ad00359a34532330cede832f07ef
Kevin Lefevre <archi@kiln.fi>
2025-12-09 09:52:28 +0100

    chore: fix docs
    Signed-off-by: Kevin Lefevre <archi@kiln.fi>

--- modules/vault-region/versions.tf
+++ modules/vault-region/versions.tf
@@ -1,6 +1,6 @@
 # Specify required provider versions below
 terraform {
-  required_version = "~> 1.3"
+  required_version = ">= 1.5.7"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
